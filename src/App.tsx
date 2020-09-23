923f93e94ff5aad02b573dedeef0e152ca65ca62
Edison Sanchez <sanchez.edison@gmail.com>
2020-09-23 19:46:15 -0400

    Added Husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = (): ReactElement => {
+const App: React.FC = (): React.ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,12 +11,7 @@ const App: React.FC = (): ReactElement => {
           Edit
           <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
