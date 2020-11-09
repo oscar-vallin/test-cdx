7d7d1727cc7306fffe82b48e9c52e66f608eba3f
Edison Sanchez <sanchez.edison@gmail.com>
2020-11-08 21:44:07 -0400

    Updating Testing Units

--- src/components/layouts/Row/Row.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Row as Component } from './Row';
-
-test('Render - Testing Message inside Row', () => {
-  const { getByText } = render(<Component>Testing</Component>);
-  const linkElement = getByText(/Test/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/containers/bars/NavBar/NavBar.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { FormLogin as Component } from "./index";
-
-test("Render - Testing Message inside FormLogin", () => {
-  const { getByText } = render(<Component />);
-  const linkElement = getByText(/Testing/i);
-  expect(linkElement).toBeInTheDocument();
-});
