b14927e09362d7fdb740227aa2b7bf86c9deda41
Edison Sanchez <sanchez.edison@gmail.com>
2021-02-17 23:09:17 -0400

    Adding Transmissions Page Service

--- src/containers/tables/TableTransmissions/TableTransmissions.service.js
+++ src/containers/tables/TableTransmissions/TableTransmissions.service.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
-import { useWorkPacketStatusesQuery } from '../../../data/services/graphql';
+import { useWpTransmissionsQuery } from '../../../data/services/graphql';
 import { getTableStructure, TABLE_NAMES } from '../../../data/constants/TableConstants';
 import { getStepStatusLabel } from '../../../data/constants/FileStatusConstants';
 import { useInputValue } from '../../../hooks/useInputValue';
@@ -12,7 +12,7 @@ export const useTable = (argOrgSid, argDateRange, argFilter) => {
   const [columns, setColumns] = useState([]);
   const structure = getTableStructure(TABLE_NAMES.ERRORS);
 
-  const { data, loading, error } = useWorkPacketStatusesQuery({
+  const { data, loading, error } = useWpTransmissionsQuery({
     variables: {
       orgSid: argOrgSid,
       dateRange: argDateRange,
@@ -27,49 +27,42 @@ export const useTable = (argOrgSid, argDateRange, argFilter) => {
 
   useEffect(() => {
     const doEffect = () => {
-      console.log('TableTransmissions.service, data:', data);
+      console.log('TableTransmissions.service, data.nodes:', data.wpTransmissions.nodes);
       const _columns = [
         { key: 'datetime', label: 'Delivered On', id: 'datetime', style: 'text' },
         { key: 'planSponsor', label: 'Plan Sponsor', id: 'planSponsor', style: 'text' },
-        { key: 'vendor', label: 'Vendor', id: 'vendor', style: 'text' },
-        { key: 'vendor', label: 'Spec', id: 'vendor', style: 'text' },
-        { key: 'vendor', label: 'Implementation', id: 'vendor', style: 'text' },
-        { key: 'clientFile', label: 'Client File', id: 'clientFile', style: 'link' },
-        { key: 'vendorFile', label: 'Vendor File', id: 'vendorFile', style: 'link' },
-        { key: 'message', label: 'Message', id: 'message', style: 'text' },
-        // { key: 'message', label: 'Outbound File Size', id: 'message', style: 'text' },
-        // { key: 'message', label: 'Billing Unit Count', id: 'message', style: 'text' },
-        // { key: 'message', label: 'Total Records', id: 'message', style: 'text' },
-        // { key: 'message', label: 'Feed', id: 'message', style: 'text' },
-        // { key: 'message', label: 'Version', id: 'message', style: 'text' },
+        { key: 'vendorId', label: 'Vendor', id: 'vendorId', style: 'text' },
+        { key: 'specId', label: 'Spec', id: 'specId', style: 'text' },
+        { key: 'implementation', label: 'Implementation', id: 'implementation', style: 'text' },
+        { key: 'inboundFilename', label: 'Client File', id: 'inboundFilename', style: 'link' },
+        { key: 'outboundFilename', label: 'Vendor File', id: 'outboundFilename', style: 'link' },
+        { key: 'outboundFilesize', label: 'Outbound File Size', id: 'outboundFilesize', style: 'text' },
+        { key: 'billingCount', label: 'Billing Unit Count', id: 'billingCount', style: 'text' },
+        { key: 'totalRecords', label: 'Total Records', id: 'totalRecords', style: 'text' },
+        { key: 'extractType', label: 'Feed', id: 'extractType', style: 'text' },
+        { key: 'extractVersion', label: 'Version', id: 'extractVersion', style: 'text' },
       ];
 
-      const _items = data.workPacketStatuses.map(
-        ({
-          timestamp,
-          clientFileArchivePath,
-          inboundFilename,
-          planSponsorId,
-          vendorId,
-          hasErrors,
-          workOrderId,
-          vendorFileArchivePath,
-        }) => {
-          const datetime = format(new Date(timestamp), 'MM/dd/yyyy hh:mm a');
-          const message = hasErrors ? 'Error' : '';
+      console.log('Received Transmission Data: ', data);
 
-          return [
-            formatField(datetime, 'text', 'datetime', datetime),
-            formatField(planSponsorId, 'text', 'planSponsor', planSponsorId),
-            formatField(vendorId, 'text', 'vendor', vendorId),
-            formatField(vendorId, 'text', 'vendor', vendorId),
-            formatField(vendorId, 'text', 'vendor', vendorId),
-            formatField(inboundFilename, 'link', 'clientFile', clientFileArchivePath),
-            formatField(workOrderId, 'link', 'vendorFile', vendorFileArchivePath),
-            formatField(message, 'text', 'message', message),
-          ];
-        }
-      );
+      const _items = data.wpTransmissions.nodes.map((item) => {
+        const datetime = format(new Date(item.deliveredOn), 'MM/dd/yyyy hh:mm a');
+
+        return [
+          formatField(datetime, 'text', 'datetime', datetime),
+          formatField(item.planSponsorId, 'text', 'planSponsor', item.planSponsorId),
+          formatField(item.vendorId, 'text', 'vendorId', item.vendorId),
+          formatField(item.specId, 'text', 'specId', item.specId),
+          formatField(item.implementation, 'text', 'implementation', item.implementation),
+          formatField(item.inboundFilename, 'link', 'inboundFilename', item.inboundFilename),
+          formatField(item.outboundFilename, 'link', 'outboundFilename', item.outboundFilename),
+          formatField(item.outboundFilesize, 'text', 'outboundFilesize', item.outboundFilesize),
+          formatField(item.billingCount, 'text', 'billingCount', item.billingCount),
+          formatField(item.totalRecords, 'text', 'totalRecords', item.totalRecords),
+          formatField(item.extractType, 'text', 'extractType', item.extractType),
+          formatField(item.extractVersion, 'text', 'extractVersion', item.extractVersion),
+        ];
+      });
 
       setColumns(_columns);
       setItems(_items);
