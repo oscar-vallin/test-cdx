514bb4078ec0902966c50e3d8b2a77c755bd7e80
Edison Sanchez <56738769+AnthonyDPP07@users.noreply.github.com>
2021-04-29 00:24:45 -0400

    dateRange is today as default

--- src/containers/tables/TableFileStatus/TableFileStatus.js
+++ src/containers/tables/TableFileStatus/TableFileStatus.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Table } from '../../../components/tables/Table';
+import { useParams } from 'react-router-dom';
+import { endOfDay, format, startOfDay } from 'date-fns';
 
 import { Box, Row, Column, Container, RightColumn } from './TableFileStatus.styles';
 import { useTable } from './TableFileStatus.service';
@@ -8,25 +10,38 @@ import { InputText } from '../../../components/inputs/InputText';
 import { InputDateRange } from '../../../components/inputs/InputDateRange';
 import { useTableFilters } from '../../../hooks/useTableFilters';
 
-const TableFileStatus = ({ id = 'TableFileStatus', orgSid = 1, dateRange, filter }) => {
+const TableFileStatus = ({ idComp = 'TableFileStatus', orgSid = 1, dateRange, filter }) => {
   const { tableProps } = useTable(orgSid, dateRange, filter);
   const { localInput, startDate, endDate } = useTableFilters('Extract Name,  Status, Vendor, etc.');
 
   console.log('TableFileStatus, localInput: ', localInput);
+  const { id } = useParams();
+  useEffect(() => {
+    if (id === undefined) {
+      const endDay = format(endOfDay(new Date()), 'MM/dd/yyyy hh:mm a');
+      const startDay = format(startOfDay(new Date()), 'MM/dd/yyyy hh:mm a');
+
+      startDate.setValue(startDay);
+      endDate.setValue(endDay);
+      return;
+    }
+  }, []);
+
+  console.log('dates: ', startDate.value, endDate.value);
   return (
     <Container>
-      <Row id={`${id}-filters`} around>
+      <Row id={`${idComp}-filters`} around>
         <Column center>
-          <InputText id={`${id}__Card__Row__Input-Email`} autoFocus disabled={false} {...localInput} />
+          <InputText id={`${idComp}__Card__Row__Input-Email`} autoFocus disabled={false} {...localInput} />
         </Column>
         <RightColumn center>
           <InputDateRange startDate={startDate} endDate={endDate} />
         </RightColumn>
       </Row>
       {!tableProps.loading && (
-        <Box id={`${id}`}>
+        <Box id={`${idComp}`}>
           <Table
-            id={`${id}`}
+            id={`${idComp}`}
             onOption={() => console.log('Table click')}
             searchInput={localInput.value}
             {...tableProps}
