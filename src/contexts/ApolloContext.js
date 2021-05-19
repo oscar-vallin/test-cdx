c3549e7a9aecf7821d4652e3bcbe2bf7e5b653fd
unknown <edison.sanchez@known2u.com>
2021-05-19 18:48:08 -0400

    210519_CDX_Enhance__Apollo_no_cache: Chaging cache and network style

--- src/contexts/ApolloContext.js
+++ src/contexts/ApolloContext.js
@@ -54,9 +54,26 @@ export const ApolloContextProvider = ({ children }) => {
     });
   });
 
+  const defaultOptions = {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+  };
+
   let client = new ApolloClient({
     link: afterwareLink.concat(authLink.concat(httpLink)),
-    cache: new InMemoryCache({ possibleTypes: introspection.possibleTypes }),
+    // cache: new InMemoryCache({ possibleTypes: introspection.possibleTypes }),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'cache-and-network',
+      },
+    },
   });
 
   // Component Did Mount
