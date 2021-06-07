5e38c1a1e4479b81716ce92ce7c3eb4d4f663dca
Edison <edison@macbook20.com>
2021-06-06 23:24:25 -0400

    210601_CDX_Feature__Query_strings: Adding Dashboard query stirngs

--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // components
 import { CardDashboard } from '../../containers/cards/CardDashboard';
 import { TableDashboard } from '../../containers/tables/TableDashboard';
@@ -8,6 +8,8 @@ import { StyledRow, StyledRowDate, StyledColumn, StyledButton, StyledSpinner } f
 import { useDashboardService } from './DashboardPage.service';
 
 import { TABLE_NAMES } from '../../data/constants/TableConstants';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import queryString from 'query-string';
 
 // TODO: Change for Session organization ID.
 const ORG_SID = 1;
@@ -16,6 +18,36 @@ const _DashboardPage = () => {
   const service = useDashboardService(ORG_SID);
   const { isLoadingData, datesOptions, dataCounters } = service;
   const { setDateId, dateId } = service;
+  const history = useHistory();
+  const location = useLocation();
+  const [urlParams, setUrlParams] = useState(queryString.parse(location.search));
+
+  useEffect(() => {
+    if (urlParams?.date) {
+      console.log('set Params in Date = ', urlParams.date);
+      setDateId(urlParams.date);
+    }
+
+    return () => {
+      return null;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (location.search) {
+      queryString.parse(location.search);
+    }
+  }, [location]);
+
+  useEffect(() => {
+    console.log('history = ', history);
+  }, []);
+
+  const handleChangeDate = (date) => {
+    setDateId(date);
+
+    history.push(`?date=${date}`);
+  };
 
   // Render Buttons Bar
   const renderDateButtons = () => {
@@ -27,7 +59,7 @@ const _DashboardPage = () => {
           key={`Button-${option.id}`}
           variant={option.selected ? 'primary' : 'secondary'}
           selected={option.selected}
-          onClick={() => setDateId(option.id)}
+          onClick={() => handleChangeDate(option.id)}
         >
           {option.name}
         </StyledButton>
