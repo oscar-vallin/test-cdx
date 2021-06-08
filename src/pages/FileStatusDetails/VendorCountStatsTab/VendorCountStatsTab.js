367de4e10ea430a484fda19417f045e57fc5ae09
Edison Sanchez <edison.sanchez@known2u.com>
2021-06-08 16:22:53 -0500

    210607_CDX_Bugfix_FileStatus_Detail_Uncatched_Error: Fix Vendor Count Stats and Quality Check tabs when values empty

--- src/pages/FileStatusDetails/VendorCountStatsTab/VendorCountStatsTab.js
+++ src/pages/FileStatusDetails/VendorCountStatsTab/VendorCountStatsTab.js
@@ -5,42 +5,44 @@ import { StyledRow, StyledVendorHeaderRow, StyledFooter } from '../FileStatusDet
 
 const COLUMNS = [
   { key: 'name', name: 'Record Name', fieldName: 'name' },
-  { key: 'count', name: 'Count', fieldName: 'count', },
-].map(col => ({ ...col, data: 'string', isPadded: true }));
+  { key: 'count', name: 'Count', fieldName: 'count' },
+].map((col) => ({ ...col, data: 'string', isPadded: true }));
 
 const onRenderRow = (props) => {
-  return <StyledRow {...props} />
-}
+  return <StyledRow {...props} />;
+};
 
-const onRenderDetailsHeader = props => {
+const onRenderDetailsHeader = (props) => {
   return (
     <StyledVendorHeaderRow>
-     {props.columns.map(column => <div>{column.name}</div>)}
+      {props.columns.map((column) => (
+        <div>{column.name}</div>
+      ))}
     </StyledVendorHeaderRow>
-  )
-}
+  );
+};
 
 const onRenderDetailsFooter = (count, props) => {
   return (
     <StyledFooter>
       <div>Total</div>
-      <div>{count}</div>
+      <div>{count || '0'}</div>
     </StyledFooter>
   );
-}
+};
 
 const VendorCountStatsTab = ({ items }) => {
   return (
     <Spacing padding="normal">
       <DetailsList
         compact
-        items={items.recordCount}
+        items={items?.recordCount || []}
         columns={COLUMNS}
         selectionMode={SelectionMode.none}
         layoutMode={DetailsListLayoutMode.justified}
         onRenderRow={onRenderRow}
         onRenderDetailsHeader={onRenderDetailsHeader}
-        onRenderDetailsFooter={(args) => onRenderDetailsFooter(items.totalCount, args)}
+        onRenderDetailsFooter={(args) => onRenderDetailsFooter(items?.totalCount, args)}
         isHeaderVisible
       />
     </Spacing>
