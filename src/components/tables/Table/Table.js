d24dfde2205f039bd9e74c953c993d1fd3c8d99c
Edison Sanchez <edison.sanchez@known2u.com>
2021-06-25 14:43:35 -0500

    210622_CDX_Bugfix__Browser_back_button: fix back buttons when there is a filter

--- src/components/tables/Table/Table.js
+++ src/components/tables/Table/Table.js
@@ -155,7 +155,7 @@ const Table = ({ items, columns, structure, onOption, groups, searchInput, date 
       setFilterInput();
       _buildItems();
     }
-  }, [searchInput]);
+  }, [searchInput, items]);
 
   useEffect(() => {
     setFilterInput(searchInput);
