1f82fc7e90f36fe6b5798b81f0b3e9ad7799565a
Gabe Colombo <gabe.colombo@known2u.com>
2021-08-01 14:04:15 -0300

    Implementing active org dropdown

--- src/pages/Admin/Organizations/ActiveOrgs/ActiveOrgsPage.js
+++ src/pages/Admin/Organizations/ActiveOrgs/ActiveOrgsPage.js
@@ -69,6 +69,10 @@ const _ActiveOrgsPage = () => {
       setOrgSid(newOrgSid);
       history.push(`${URL_ROUTES.FILE_STATUS}?orgSid=${newOrgSid}`);
     }, 1000);
+
+    storeOrgsId(newOrgSid);
+
+    history.push(URL_ROUTES.FILE_STATUS);
   };
 
   const onRenderItemColumn = (item, index, column) => {
