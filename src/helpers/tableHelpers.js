31fd5bfaaae3068188179e5242564c02dcbf0c1a
Edison Sanchez <edison.sanchez@known2u.com>
2021-09-20 23:23:56 -0500

    restore min dates and added validation to ensure StartDate is less than or equal to the EndDate.

--- src/helpers/tableHelpers.js
+++ src/helpers/tableHelpers.js
@@ -33,7 +33,7 @@ export const isTodayInRange = (firstDate, secondDate) => {
   const _endDate = endOfDay(new Date(secondDate));
 
   if (firstDate && secondDate) {
-    if (firstDate < secondDate) {
+    if (firstDate <= secondDate) {
       return isWithinInterval(_today, { start: new Date(_startDate), end: new Date(_endDate) });
     }
     return false;
