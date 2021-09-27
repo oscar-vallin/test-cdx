80b7378a42b598e711f5c86c5766ab69b0538962
Edison Sanchez <edison.sanchez@known2u.com>
2021-09-27 18:11:12 -0500

    added tests to button

--- src/components/buttons/Button/Button.test.tsx
+++ src/components/buttons/Button/Button.test.tsx
@@ -2,16 +2,17 @@ import { shallow } from 'enzyme';
 import { Button } from './Button.js';
 
 const defaultProps = {
-  text: 'text',
+  id: '__Button',
+  text: 'Button Text',
+  children: null,
+  variant: 'secondary',
+  disabled: false,
+  onClick: jest.fn(),
 };
 
 describe('Button Testing Unit...', () => {
   const mockFn = jest.fn();
-  const tree = shallow(
-    <Button {...defaultProps} onClick={mockFn}>
-      Testing Button
-    </Button>
-  );
+  const tree = shallow(<Button {...defaultProps} onClick={mockFn}></Button>);
 
   it('Should be defined', () => {
     expect(Button).toBeDefined();
@@ -24,5 +25,26 @@ describe('Button Testing Unit...', () => {
   it('Should have a click function', () => {
     tree.simulate('click');
     expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('Should have a props', () => {
+    expect(tree.props().id).toEqual('__Button');
+    expect(tree.props().variant).toEqual('secondary');
+    expect(tree.props().disabled).toBeFalsy();
+  });
+
+  it('Should have a children with only text when text is passed', () => {
+    expect(tree.text()).toEqual('Button Text');
+  });
+
+  it('Should have a children when it is passed', () => {
+    const tree = shallow(<Button {...defaultProps} text={null} children={<p>Text Children</p>}></Button>);
+    expect(tree.children().text()).toEqual('Text Children');
+  });
+
+  it("Can't click when button is disabled", () => {
+    const tree = shallow(<Button {...defaultProps} disabled={true}></Button>);
+    tree.simulate('click');
+    expect(mockFn).not.toHaveBeenCalled();
   });
 });
