2ad66d50c6a05b8b7e5e4cfea503f52b4d79585d
Gabe Colombo <gabe.colombo@known2u.com>
2021-10-10 13:47:33 -0300

    Rewriting policy form

--- src/pages/Admin/AccessManagement/Policies/CreatePolicy/CreatePoliciesPanel.js
+++ src/pages/Admin/AccessManagement/Policies/CreatePolicy/CreatePoliciesPanel.js
@@ -26,7 +26,7 @@ import { useQueryHandler } from '../../../../../hooks/useQueryHandler';
 import { StyledCommandButton, StyledColumn } from './CreatePoliciesPanel.styles';
 
 import {
-  useAccessPolicyFormQuery,
+  useAccessPolicyFormLazyQuery,
   useCreateAccessPolicyMutation,
   useUpdateAccessPolicyMutation,
   useAccessPolicyLazyQuery,
@@ -80,52 +80,38 @@ const groupPermissions = ([permissions, ...opts]) => {
 const CreatePoliciesPanel = ({ isOpen, onDismiss, onCreatePolicy, selectedPolicyId }) => {
   const { orgSid } = useOrgSid();
   const [state, setState] = useState({ ...INITIAL_STATE });
+  const [policyForm, setPolicyForm] = useState({});
+  const [permissions, setPermissions] = useState([]);
 
-  const [options, setOptions] = useState({ ...INITIAL_OPTIONS });
-
-  const [apiUseAccessPolicyForm, { data }] = useAccessPolicyFormQuery();
+  const [fetchPolicyForm, { data: form }] = useQueryHandler(useAccessPolicyFormLazyQuery);
   const [createPolicy, { data: createdPolicy, loading: isCreatingPolicy }] = useCreateAccessPolicyMutation();
-  const [fetchPolicy, { data: policy }] = useAccessPolicyLazyQuery();
+  // const [fetchPolicy, { data: policy }] = useAccessPolicyLazyQuery();
   const [updatePolicy] = useUpdateAccessPolicyMutation();
 
-  useEffect(() => {
-    if (isOpen && selectedPolicyId) {
-      fetchPolicy({
-        variables: {
-          orgSid,
-          policySid: selectedPolicyId,
-        },
-      });
-    }
-  }, [selectedPolicyId, isOpen]);
+  // useEffect(() => {
+  //   if (isOpen && selectedPolicyId) {
+  //     fetchPolicy({
+  //       variables: {
+  //         orgSid,
+  //         policySid: selectedPolicyId,
+  //       },
+  //     });
+  //   }
+  // }, [selectedPolicyId, isOpen]);
 
-  useEffect(() => {
-    if (policy) {
-      const { amPolicy } = policy;
+  // useEffect(() => {
+  //   if (policy) {
+  //     const { amPolicy } = policy;
 
-      setState({
-        ...state,
-        policySid: amPolicy.id,
-        policyName: amPolicy.name,
-        isTemplate: amPolicy.tmpl,
-        usedAsIs: amPolicy.tmplUseAsIs,
-        serviceType: amPolicy.tmplServiceType,
-        permissions: (amPolicy.permissions || []).map((permission) => ({
-          policySid: amPolicy.id,
-          effect: permission.effect,
-          predicateName: permission.predicate,
-          parameterVariable: permission.predVar1,
-          parameterValue: permission.predParam1,
-          actions: (permission.actions || []).map((action) => ({
-            facet: { key: action.facet },
-            service: { key: action.service },
-            verb: { key: action.verb },
-            permissionSid: permission.id,
-          })),
-        })),
-      });
-    }
-  }, [policy]);
+  //     setState({
+  //       ...state,
+  //       policySid: amPolicy.id,
+  //       policyName: amPolicy.name,
+  //       isTemplate: amPolicy.tmpl,
+  //       usedAsIs: amPolicy.tmplUseAsIs,
+  //     });
+  //   }
+  // }, [policy]);
 
   // useEffect(() => {
   //   if (policy) {
