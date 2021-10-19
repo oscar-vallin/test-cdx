12627490d45a1eaeeeb1ec249a2326cfb5f906d7
Edison Sanchez <edison.sanchez@known2u.com>
2021-10-19 13:53:52 -0500

    WIP Edit policy group

--- src/pages/Admin/AccessManagement/Groups/AccessManagementGroupsPage.styles.js
+++ src/pages/Admin/AccessManagement/Groups/AccessManagementGroupsPage.styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
+import { CommandBarButton } from '@fluentui/react';
 import { Column } from '../../../../components/layouts';
+
+export const StyledCommandButton = styled(CommandBarButton)`
+  background: transparent;
+`;
 
 export const StyledColumn = styled(Column)`
   .ms-Viewport {
