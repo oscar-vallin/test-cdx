843ff8708a781ebcd8a71634a942bc52560c7bcd
Edison Sanchez <edison.sanchez@known2u.com>
2021-10-20 16:23:15 -0500

    changed images components from js to ts

--- src/components/images/Image/Image.tsx
+++ src/components/images/Image/Image.tsx
@@ -1,32 +1,20 @@
-import { ReactElement } from 'react';
+import PropTypes from 'prop-types';
 import { StyledImage } from './Image.styles';
 
 import imgLogo from '../../../assets/images/known2u-logo.png';
-
-const defaultProps = {
-  id: '',
-  name: '',
-  src: '',
-  alt: '',
-};
-
-type ImageProps = {
-  id?: string;
-  name?: string;
-  src?: string;
-  alt?: string;
-} & typeof defaultProps;
 
 const images = {
   logo: imgLogo,
 };
 
-const Image = ({ id, name, src, alt }: ImageProps): ReactElement => {
+const Image = ({ id, name, src, alt }) => {
   const _src = images[name] ?? src;
 
   return <StyledImage id={id} src={_src} alt={alt} />;
 };
 
-Image.defaultProps = defaultProps;
+Image.propTypes = {
+  id: PropTypes.string,
+};
 
 export { Image };
