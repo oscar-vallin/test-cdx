89eb67263cbd656d7b3fe09ac32facd935cf1f0c
Edison Sanchez <edison.sanchez@known2u.com>
2021-10-20 15:19:43 -0500

    changed cards components from js to ts

--- src/components/cards/Card/Card.tsx
+++ src/components/cards/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import { getClassNames } from '../../../helpers/helperStyles';
 import { CardStyled } from './Card.styles';
 
