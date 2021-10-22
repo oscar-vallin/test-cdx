0e3ab1bfbcd3ec425bdbe92941b8f5d2ecfcca8a
Edison Sanchez <edison.sanchez@known2u.com>
2021-10-22 10:53:34 -0500

    added input types and return types

--- src/components/layouts/Container/Container.tsx
+++ src/components/layouts/Container/Container.tsx
@@ -1,7 +1,18 @@
+import { ReactElement, ReactNode } from 'react';
 import { StyledBox } from './Container.styles';
 
-const Container = ({ children, ...props }) => {
+const Container = ({ children, ...props }: ContainerProps): ReactElement => {
   return <StyledBox {...props}>{children}</StyledBox>;
+};
+
+type ContainerProps = {
+  id: string;
+  children: ReactNode;
+  direction: string;
+  left: string;
+  right: string;
+  top: string;
+  bottom: string;
 };
 
 export { Container };
