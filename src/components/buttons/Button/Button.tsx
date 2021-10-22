12d4f38bd6f1d50aaafecf807f05d1f1a3974c39
Edison Sanchez <edison.sanchez@known2u.com>
2021-10-22 16:51:09 -0500

    Button component with typescritp

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,21 +1,40 @@
-import PropTypes from 'prop-types';
+import { ReactElement, ReactNode } from 'react';
 import { StyledButton } from './Button.styles';
 
-const Button = ({ id, text, children, variant = 'secondary', disabled = false, onClick, block = false, ...props }) => {
+const defaultProps = {
+  id: '',
+  text: '',
+  variant: 'secondary',
+  disabled: false,
+  onClick: () => null,
+  block: false,
+};
+
+type ButtonProps = {
+  id?: string;
+  text?: string | null;
+  children?: ReactNode;
+  variant: string;
+  disabled: boolean;
+  onClick?: () => null;
+  block: boolean;
+} & typeof defaultProps;
+
+const Button = ({
+  id,
+  text,
+  children,
+  variant = 'secondary',
+  disabled = false,
+  onClick,
+  block = false,
+  ...props
+}: ButtonProps): ReactElement => {
   return (
     <StyledButton id={id} variant={variant} disabled={disabled} onClick={onClick} block={block} {...props}>
       {text || children}
     </StyledButton>
   );
-};
-
-Button.propTypes = {
-  id: PropTypes.string,
-  children: PropTypes.node,
-  variant: PropTypes.string,
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
-  block: PropTypes.bool,
 };
 
 export { Button };
