072704f0070426f0e6906158e5718f7ae5425209
Edison Sanchez <edison.sanchez@known2u.com>
2021-11-11 10:31:53 -0500

    Modifying internal routes

--- internals/route-extractor.mjs
+++ internals/route-extractor.mjs
@@ -3,14 +3,13 @@
 import fs from 'fs';
 import path from 'path';
 //import chalk from 'chalk';
-import { URL_ROUTES } from './../src/data/constants/RouteConstants.js';
+import { URL_ROUTES } from './../src/data/constants/RouteConstants';
 
 (async () => {
   //console.log(chalk.blue('Extracting top level routes (URL_ROUTES) from RouteContants.js'))
 
-  const routes = Object
-    .entries(URL_ROUTES)
-    .filter(([key, value]) => value.split('/').filter(val => val !== '').length === 1)
+  const routes = Object.entries(URL_ROUTES)
+    .filter(([key, value]) => value.split('/').filter((val) => val !== '').length === 1)
     .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {});
 
   //console.log(chalk.yellow('Parsed routes:', JSON.stringify(routes)))
@@ -19,7 +18,7 @@ import { URL_ROUTES } from './../src/data/constants/RouteConstants.js';
     fs.writeFileSync('./internals/parent-routes.json', JSON.stringify(routes));
 
     //console.log(chalk.green('Routes extracted successfully to /internals/parent-routes.json'))
-  } catch(err) {
+  } catch (err) {
     //console.log(chalk.red('An error occurred while creating parent-routes.json'), err.message || err);
   }
 })();
