44322e6d9a10d3ecf331045d80a3aa9fdd4f108f
Edison Sanchez <edison.sanchez@known2u.com>
2021-12-30 01:32:31 -0500

    Removing temporally tests from AccessManagementGroupsPage

--- src/pages/Admin/AccessManagement/Groups/AccessManagementGroupsPage.e2e.test.ts
+++ src/pages/Admin/AccessManagement/Groups/AccessManagementGroupsPage.e2e.test.ts
@@ -30,56 +30,56 @@ describe('E2E - Access Policy Group Test', () => {
     await accessCreateGroup.clickOnCreateGroup();
   });
 
-  it('Create Group with template Cancel', async () => {
-    const selector = '#__AccessManagement__Name_Field_2';
-    const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
-    const templeteCheckMark = 'i[data-icon-name="Cancel"]';
+  // it('Create Group with template Cancel', async () => {
+  //   const selector = '#__AccessManagement__Name_Field_2';
+  //   const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
+  //   const templeteCheckMark = 'i[data-icon-name="Cancel"]';
 
-    await accessCreateGroup.createGroupTempleteCancel();
-    const actualText = await cdxApp.page.$eval(templeteCheckMark, (e) => e.getAttribute('data-icon-name'));
-    expect(actualText).toEqual('Cancel');
-    await accessCreateGroup.expectTextOnPage(selector, 'CDX E2E Group');
-  });
+  //   await accessCreateGroup.createGroupTempleteCancel();
+  //   const actualText = await cdxApp.page.$eval(templeteCheckMark, (e) => e.getAttribute('data-icon-name'));
+  //   expect(actualText).toEqual('Cancel');
+  //   await accessCreateGroup.expectTextOnPage(selector, 'CDX E2E Group');
+  // });
 
-  it('Delete Group with template Cancel', async () => {
-    const selector = '#__AccessManagement__Name_Field_1';
-    const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
+  // it('Delete Group with template Cancel', async () => {
+  //   const selector = '#__AccessManagement__Name_Field_1';
+  //   const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
 
-    await accessCreateGroup.expectSpanText();
-    await accessCreateGroup.deleteGroup('2');
-    await accessCreateGroup.expectTextOnPage(selector, 'CDX Super User Group');
-  });
+  //   await accessCreateGroup.expectSpanText();
+  //   await accessCreateGroup.deleteGroup('2');
+  //   await accessCreateGroup.expectTextOnPage(selector, 'CDX Super User Group');
+  // });
 
-  it('Create Group with template CheckMark', async () => {
-    const selector = '#__AccessManagement__Name_Field_2';
-    const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
-    const templeteCheckMark = 'i[data-icon-name="CheckMark"]';
+  // it('Create Group with template CheckMark', async () => {
+  //   const selector = '#__AccessManagement__Name_Field_2';
+  //   const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
+  //   const templeteCheckMark = 'i[data-icon-name="CheckMark"]';
 
-    await accessCreateGroup.createGroup();
-    const actualText = await cdxApp.page.$eval(templeteCheckMark, (e) => e.getAttribute('data-icon-name'));
-    expect(actualText).toEqual('CheckMark');
-    await accessCreateGroup.expectTextOnPage(selector, 'CDX E2E Group');
-  });
+  //   await accessCreateGroup.createGroup();
+  //   const actualText = await cdxApp.page.$eval(templeteCheckMark, (e) => e.getAttribute('data-icon-name'));
+  //   expect(actualText).toEqual('CheckMark');
+  //   await accessCreateGroup.expectTextOnPage(selector, 'CDX E2E Group');
+  // });
 
-  it('Update Group', async () => {
-    const selector = '#__AccessManagement__Name_Field_2';
-    const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
-    const templeteCheckMark = 'i[data-icon-name="Cancel"]';
+  // it('Update Group', async () => {
+  //   const selector = '#__AccessManagement__Name_Field_2';
+  //   const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
+  //   const templeteCheckMark = 'i[data-icon-name="Cancel"]';
 
-    await accessCreateGroup.updateCheckBoxGroup();
-    const actualText = await cdxApp.page.$eval(templeteCheckMark, (e) => e.getAttribute('data-icon-name'));
-    expect(actualText).toEqual('Cancel');
-    await accessCreateGroup.expectTextOnPage(selector, 'CDX E2E Group');
-  });
+  //   await accessCreateGroup.updateCheckBoxGroup();
+  //   const actualText = await cdxApp.page.$eval(templeteCheckMark, (e) => e.getAttribute('data-icon-name'));
+  //   expect(actualText).toEqual('Cancel');
+  //   await accessCreateGroup.expectTextOnPage(selector, 'CDX E2E Group');
+  // });
 
-  it('Delete Group', async () => {
-    const selector = '#__AccessManagement__Name_Field_1';
-    const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
+  // it('Delete Group', async () => {
+  //   const selector = '#__AccessManagement__Name_Field_1';
+  //   const accessCreateGroup = new PuppetAccessPolicyGroup(cdxApp.page);
 
-    await accessCreateGroup.expectSpanText();
-    await accessCreateGroup.deleteGroup('2');
-    await accessCreateGroup.expectTextOnPage(selector, 'CDX Super User Group');
-  });
+  //   await accessCreateGroup.expectSpanText();
+  //   await accessCreateGroup.deleteGroup('2');
+  //   await accessCreateGroup.expectTextOnPage(selector, 'CDX Super User Group');
+  // });
 
   afterAll(async () => {
     await cdxApp.closeBrowser();
