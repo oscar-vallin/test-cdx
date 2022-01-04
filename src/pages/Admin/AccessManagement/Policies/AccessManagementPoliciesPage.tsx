9759182b6daa12c9bfee3e762753cd3f978a067a
Chris Fong <chris.fong@known2u.com>
2022-01-04 17:08:48 -0500

    remove the selected policy id printing on the page. Fixed Imports.

--- src/pages/Admin/AccessManagement/Policies/AccessManagementPoliciesPage.tsx
+++ src/pages/Admin/AccessManagement/Policies/AccessManagementPoliciesPage.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, memo } from 'react';
 
-import { PrimaryButton, DefaultButton, MessageBar } from 'office-ui-fabric-react';
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react';
 import { DetailsList, DetailsListLayoutMode, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
 import { Dialog, DialogType, DialogFooter } from '@fluentui/react/lib-commonjs/Dialog';
 
@@ -10,24 +10,23 @@ import { Spinner } from 'office-ui-fabric-react/lib/Spinner';
 import { EmptyState } from 'src/containers/states';
 import { SpinnerSize } from '@fluentui/react';
 import { useNotification } from 'src/hooks/useNotification';
-import { LayoutAdmin } from '../../../../layouts/LayoutAdmin';
-import { Spacing } from '../../../../components/spacings/Spacing';
-import { Button } from '../../../../components/buttons';
-import { Row, Column } from '../../../../components/layouts';
-import { Separator } from '../../../../components/separators/Separator';
-import { Text } from '../../../../components/typography';
-import { CreatePoliciesPanel } from './CreatePolicy';
-import { Link } from '../../../../components/buttons/Link';
+import { LayoutAdmin } from 'src/layouts/LayoutAdmin';
+import { Spacing } from 'src/components/spacings/Spacing';
+import { Button, Link } from 'src/components/buttons';
+import { Row, Column } from 'src/components/layouts';
+import { Separator } from 'src/components/separators/Separator';
+import { Text } from 'src/components/typography';
 
 import {
   useAccessPolicyTemplatesLazyQuery,
   useAccessPoliciesForOrgLazyQuery,
   useDeleteAccessPolicyMutation,
-} from '../../../../data/services/graphql';
+} from 'src/data/services/graphql';
 
+import { useOrgSid } from 'src/hooks/useOrgSid';
+import { useQueryHandler } from 'src/hooks/useQueryHandler';
+import { CreatePoliciesPanel } from './CreatePolicy';
 import { StyledColumn, StyledCommandButton } from '../AccessManagement.styles';
-import { useOrgSid } from '../../../../hooks/useOrgSid';
-import { useQueryHandler } from '../../../../hooks/useQueryHandler';
 
 const generateColumns = () => {
   const createColumn = ({ name, key }) => ({
@@ -247,8 +246,6 @@ const _AccessManagementPoliciesPage = () => {
             </StyledColumn>
           </Row>
         </Spacing>
-
-        {selectedPolicyId}
 
         <CreatePoliciesPanel
           isOpen={isPanelOpen}
