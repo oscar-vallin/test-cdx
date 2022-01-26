33c98ef971c91075ec28576f350734f711c29c83
Chris Fong <chris.fong@known2u.com>
2022-01-26 17:16:55 -0500

    Removed unused components

--- src/containers/menus/MainMenu/MainMenu.service.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-// useEmailField
-export const useMainMenu = (defaultOption) => {
-  const [selectedOption, setOption] = useState(defaultOption);
-
-  return { selectedOption, setOption };
-};
--- src/containers/menus/MainMenu/index.ts
+++
@@ -1,3 +0,0 @@
-import { MainMenu } from './MainMenu';
-
-export { MainMenu };
