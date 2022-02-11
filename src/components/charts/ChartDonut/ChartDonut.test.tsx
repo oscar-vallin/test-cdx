39c0cc6781bb42049daedb600c7b80af61c76469
Chris Fong <chris.fong@known2u.com>
2022-02-11 17:28:51 -0500

    Fixed test to use theming.

--- src/components/charts/ChartDonut/ChartDonut.test.tsx
+++ src/components/charts/ChartDonut/ChartDonut.test.tsx
@@ -1,6 +1,7 @@
 import { render, shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import { ChartDonut as Component } from './index';
+import { renderWithTheme } from 'src/utils/testUtils';
 
 const defaultProps = {
   id: '__ChartDonut',
@@ -24,13 +25,13 @@ test('Matches Snapshot', () => {
 
 describe('Basic Input Component', () => {
   it('Renders 6 sectors circles in simple PieChart', () => {
-    const wrapper = render(<Component {...defaultProps} />);
+    const wrapper = renderWithTheme(<Component {...defaultProps} />);
 
     expect(wrapper.find('.recharts-pie-sector').length).toEqual(defaultProps.data.length);
   });
 
   it('Renders 6 sectors circles when add Cell to specified props of eact slice', () => {
-    const wrapper = render(<Component {...defaultProps} />);
+    const wrapper = renderWithTheme(<Component {...defaultProps} />);
 
     expect(wrapper.find('.recharts-pie-sector').length).toEqual(6);
   });
@@ -44,7 +45,7 @@ describe('Basic Input Component', () => {
       { name: 'Group E', value: 0, color: '' },
       { name: 'Group F', value: 0, color: '' },
     ];
-    const wrapper = render(<Component {...defaultProps} data={emptyData} />);
+    const wrapper = renderWithTheme(<Component {...defaultProps} data={emptyData} />);
 
     expect(wrapper.find('.recharts-text.label').text()).toEqual('');
     expect(wrapper.find('.recharts-pie-sector').length).toEqual(0);
@@ -56,7 +57,7 @@ describe('Basic Input Component', () => {
   });
 
   it('Renders label legend item when add a Label element', () => {
-    const wrapper = render(<Component {...defaultProps} />);
+    const wrapper = renderWithTheme(<Component {...defaultProps} />);
     expect(wrapper.find('.recharts-text.label').text()).toEqual(defaultProps.label);
   });
 });
