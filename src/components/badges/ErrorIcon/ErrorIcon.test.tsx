394c2c7ec913457bbfdbc7ff8a2dc283d631b295
Chris Fong <chris.fong@known2u.com>
2022-02-18 17:53:40 -0500

    Updated test to account for use of them colors with the ErrorIcon component.

--- src/components/badges/ErrorIcon/ErrorIcon.test.tsx
+++ src/components/badges/ErrorIcon/ErrorIcon.test.tsx
@@ -1,22 +1,22 @@
 import { ErrorIcon } from 'src/components/badges/ErrorIcon/ErrorIcon';
-import { mount, shallow } from 'enzyme';
+import { mountWithTheme, shallowWithTheme } from 'src/utils/testUtils';
 
 
 describe('Error Icon component', () => {
 
   it('Empty value', () => {
-    const wrapper = shallow(<ErrorIcon id='icon' errorMessage={undefined} />);
-    expect(wrapper.children()).toHaveLength(0)
+    const wrapper = shallowWithTheme(<ErrorIcon id='icon' errorMessage={undefined} />);
+    expect(wrapper.html()).toEqual("");
   });
 
   it('Tooltip with left pad defaulted', () => {
-    const wrapper = mount(<ErrorIcon id='err' errorMessage='Error Message'/>);
+    const wrapper = mountWithTheme(<ErrorIcon id='err' errorMessage='Error Message'/>);
     expect(wrapper.find('FontIcon')).toHaveLength(1);
     expect(wrapper.html()).toContain('&nbsp;');
   });
 
   it('Tooltip with no left pad', () => {
-    const wrapper = mount(<ErrorIcon id='err' errorMessage='Error Message' leftPad={false}/>);
+    const wrapper = mountWithTheme(<ErrorIcon id='err' errorMessage='Error Message' leftPad={false}/>);
     expect(wrapper.find('FontIcon')).toHaveLength(1);
     expect(wrapper.html().indexOf('&nbsp;')).toBeLessThan(0);
   });
