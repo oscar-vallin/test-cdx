a31a749a81c3ea22d920f1fe7e7bc8ec3493fc56
Chris Fong <chris.fong@known2u.com>
2022-06-21 10:54:39 -0400

    Updated the test to actually test something.

--- src/containers/schedule/Schedule/ScheduleHeader.test.tsx
+++ src/containers/schedule/Schedule/ScheduleHeader.test.tsx
@@ -1,65 +1,123 @@
-import { mountWithTheme, shallowWithTheme } from 'src/utils/testUtils';
-import { ScheduleHeader as Component } from './ScheduleHeader';
+import { mountWithTheme } from 'src/utils/testUtils';
+import { ScheduleHeader } from './ScheduleHeader';
+
+
+const onChangeView = jest.fn();
 
 const defaultProps = {
   id: '',
   currentView: 'month',
   currentDate: new Date(),
   selectedDate: new Date(),
-  onChangeDate: () => null,
-  onChangeView: () => null,
+  onChangeView,
 };
 
+const MONTH_BUTTON = 'Button[id="__HeaderButtonView_Button-MonthView_Id"]';
+const MONTH_TITLE_BUTTON = 'Button[id="__MonthButtonTitle_Id"]';
+const WEEK_BUTTON = 'Button[id="__HeaderButtonView_Button-WeekView_Id"]';
+const WEEK_TITLE_BUTTON = 'Button[id="__WeekButtonTitle_Id"]';
+const DAY_BUTTON = 'Button[id="__HeaderButtonView_Button-DayView_Id"]';
+const DAY_TITLE_BUTTON = 'Button[id="__DayButtonTitle_Id"]';
+
+const MONTH_PICKER = 'MonthPicker';
+const TODAY_BUTTON = 'ButtonAction[id="__Today_Button"]';
+const PREV_MONTH_BUTTON = 'ButtonAction[id="ButtonUp"]';
+const NEXT_MONTH_BUTTON = 'ButtonAction[id="ButtonDown"]';
+const PREV_WEEK_BUTTON = 'ButtonAction[id="ButtonPrev"]';
+const NEXT_WEEK_BUTTON = 'ButtonAction[id="ButtonNext"]';
+const PREV_DAY_BUTTON = 'ButtonAction[id="ButtonPrev"]';
+const NEXT_DAY_BUTTON = 'ButtonAction[id="ButtonNext"]';
+
 describe('ButtonAction', () => {
-  const themedComponent = shallowWithTheme(<Component {...defaultProps} />);
-  const themedMount = mountWithTheme(<Component {...defaultProps} />);
 
-  it('Should be defined', () => {
-    expect(themedComponent).toBeDefined();
+  it ('Month View', () => {
+    const onChangeDate = jest.fn();
+
+    const wrapper = mountWithTheme(<ScheduleHeader {...defaultProps} onChangeDate={onChangeDate} />);
+
+    // Month button should be selected
+    expect(wrapper.find(MONTH_BUTTON).props().selected).toEqual(true);
+    expect(wrapper.find(WEEK_BUTTON).props().selected).toEqual(false);
+    expect(wrapper.find(DAY_BUTTON).props().selected).toEqual(false);
+
+    expect(wrapper.find(MONTH_TITLE_BUTTON)).toHaveLength(1);
+    expect(wrapper.find(WEEK_TITLE_BUTTON)).toHaveLength(0);
+    expect(wrapper.find(DAY_TITLE_BUTTON)).toHaveLength(0);
+
+    wrapper.find(PREV_MONTH_BUTTON).simulate('click');
+    expect(onChangeDate).toHaveBeenCalledTimes(1);
+    wrapper.find(NEXT_MONTH_BUTTON).simulate('click');
+    expect(onChangeDate).toHaveBeenCalledTimes(2);
+
+    // Clicking the month title button should show a month picker
+    expect(wrapper.find(MONTH_PICKER)).toHaveLength(0);
+    wrapper.find(MONTH_TITLE_BUTTON).simulate('click');
+    expect(wrapper.find(MONTH_PICKER)).toHaveLength(1);
+
+    wrapper.find(MONTH_BUTTON).simulate('click');
+    expect(onChangeView).toHaveBeenCalledTimes(0);
+    wrapper.find(WEEK_BUTTON).simulate('click');
+    expect(onChangeView).toHaveBeenCalled();
   });
 
-  it('Header Views, Should be defined', () => {
-    const DayContainer = mountWithTheme(<Component {...defaultProps} currentView="day" />);
-    const WeekContainer = mountWithTheme(<Component {...defaultProps} currentView="week" />);
+  it ('Week View', () => {
+    const onChangeDate = jest.fn();
 
-    expect(DayContainer).toMatchSnapshot();
-    expect(WeekContainer).toMatchSnapshot();
-    expect(themedMount).toMatchSnapshot();
+    const wrapper = mountWithTheme(<ScheduleHeader {...defaultProps} currentView="week" onChangeDate={onChangeDate}/>);
 
-    expect(DayContainer).toBeDefined();
-    expect(WeekContainer).toBeDefined();
-    expect(themedMount).toBeDefined();
+    wrapper.find(WEEK_BUTTON).simulate('click');
+    expect(wrapper.find(MONTH_BUTTON).props().selected).toEqual(false);
+    expect(wrapper.find(WEEK_BUTTON).props().selected).toEqual(true);
+    expect(wrapper.find(DAY_BUTTON).props().selected).toEqual(false);
+
+    expect(wrapper.find(MONTH_TITLE_BUTTON)).toHaveLength(0);
+    expect(wrapper.find(WEEK_TITLE_BUTTON)).toHaveLength(1);
+    expect(wrapper.find(DAY_TITLE_BUTTON)).toHaveLength(0);
+
+    wrapper.find(PREV_WEEK_BUTTON).simulate('click');
+    expect(onChangeDate).toHaveBeenCalledTimes(1);
+    wrapper.find(NEXT_WEEK_BUTTON).simulate('click');
+    expect(onChangeDate).toHaveBeenCalledTimes(2);
+
+    // Clicking the week title button should show a month picker
+    expect(wrapper.find(MONTH_PICKER)).toHaveLength(0);
+    wrapper.find(WEEK_TITLE_BUTTON).simulate('click');
+    expect(wrapper.find(MONTH_PICKER)).toHaveLength(1);
   });
 
-  it('Should sink in the button MonthView', () => {
-    const themedComponent = mountWithTheme(<Component {...defaultProps} />);
+  it ('Day View', () => {
+    const onChangeDate = jest.fn();
 
-    const btns = themedComponent.find('button[id="__HeaderButtonView_Button-MonthView_Id"]');
-    btns.simulate('click');
-    expect(btns.length).toBe(1);
+    const wrapper = mountWithTheme(<ScheduleHeader {...defaultProps} currentView="day" onChangeDate={onChangeDate}/>);
+
+    wrapper.find(DAY_BUTTON).simulate('click');
+    expect(wrapper.find(MONTH_BUTTON).props().selected).toEqual(false);
+    expect(wrapper.find(WEEK_BUTTON).props().selected).toEqual(false);
+    expect(wrapper.find(DAY_BUTTON).props().selected).toEqual(true);
+
+    expect(wrapper.find(MONTH_TITLE_BUTTON)).toHaveLength(0);
+    expect(wrapper.find(WEEK_TITLE_BUTTON)).toHaveLength(0);
+    expect(wrapper.find(DAY_TITLE_BUTTON)).toHaveLength(1);
+
+    wrapper.find(PREV_DAY_BUTTON).simulate('click');
+    expect(onChangeDate).toHaveBeenCalledTimes(1);
+    wrapper.find(NEXT_DAY_BUTTON).simulate('click');
+    expect(onChangeDate).toHaveBeenCalledTimes(2);
+
+    // Clicking the week title button should show a month picker
+    expect(wrapper.find(MONTH_PICKER)).toHaveLength(0);
+    wrapper.find(DAY_TITLE_BUTTON).simulate('click');
+    expect(wrapper.find(MONTH_PICKER)).toHaveLength(1);
   });
 
-  it('Should sink in the button HeaderTitle', () => {
-    const themedComponent = mountWithTheme(<Component {...defaultProps} />);
+  it ('Today Button', () => {
+    const onChangeDate = jest.fn();
 
-    const btns = themedComponent.find('button[id="__MonthButtonTitle_Id"]');
-    btns.simulate('click');
-    expect(btns.length).toBe(1);
+    const wrapper = mountWithTheme(<ScheduleHeader {...defaultProps} onChangeDate={onChangeDate} />);
+
+    expect(onChangeDate).not.toBeCalled();
+    wrapper.find(TODAY_BUTTON).simulate('click');
+    expect(onChangeDate).toBeCalled();
   });
 
-  it('Should sink in the button WeekView', () => {
-    const themedComponent = mountWithTheme(<Component {...defaultProps} />);
-
-    const btns = themedComponent.find('button[id="__HeaderButtonView_Button-WeekView_Id"]');
-    btns.simulate('click');
-    expect(btns.length).toBe(1);
-  });
-
-  it('Should sink in the button DayView', () => {
-    const themedComponent = mountWithTheme(<Component {...defaultProps} />);
-
-    const btns = themedComponent.find('button[id="__HeaderButtonView_Button-DayView_Id"]');
-    btns.simulate('click');
-    expect(btns.length).toBe(1);
-  });
 });
