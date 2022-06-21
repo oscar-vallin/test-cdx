4e97180f5f61d9921575473bd2529b1761b6d633
Chris Fong <chris.fong@known2u.com>
2022-06-21 15:02:05 -0400

    Refactored and removed duplicate code.

--- src/pages/Admin/Users/CreateUsers/SectionSummary.tsx
+++ src/pages/Admin/Users/CreateUsers/SectionSummary.tsx
@@ -1,34 +1,14 @@
 import { ReactElement } from 'react';
-
-import { UserAccountForm } from 'src/data/services/graphql';
 import { SummaryWizardBody } from 'src/pages/Admin/Users/SummaryWizardBody';
+import { SectionSummaryPropsType } from 'src/pages/Admin/Users/WizardTypes';
 import CreateUsersFooter from './CreateUsersFooter';
 
-type SectionSummaryPropsType = {
-  form: UserAccountForm;
-  onPrev: () => void;
-  onSubmit: () => void;
-  isProcessing?: boolean;
-};
-
 const SectionSummary = ({ form, onPrev, onSubmit, isProcessing }: SectionSummaryPropsType): ReactElement => {
-  const handlePrev = () => {
-    onPrev();
-
-    return null;
-  };
-
-  const handleSubmit = () => {
-    onSubmit();
-
-    return null;
-  };
-
   return (
     <>
       <SummaryWizardBody form={form} />
       {isProcessing && <>Processing</>}
-      {!isProcessing && <CreateUsersFooter onPrev={handlePrev} onSubmit={handleSubmit} />}
+      {!isProcessing && <CreateUsersFooter onPrev={onPrev} onSubmit={onSubmit} />}
     </>
   );
 };
