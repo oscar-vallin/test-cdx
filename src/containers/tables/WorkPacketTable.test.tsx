65a5431e6012ce8c3aaf869517e67dcd38019726
Chris Fong <chris.fong@known2u.com>
2022-06-24 18:44:27 -0400

    Added additional tests for the WorkPacketTable component

--- src/containers/tables/WorkPacketTable.test.tsx
+++ src/containers/tables/WorkPacketTable.test.tsx
@@ -1,15 +1,129 @@
 import { WorkPacketTable } from './WorkPacketTable';
-import { shallowWithTheme } from 'src/utils/testUtils';
+import { mountWithTheme } from 'src/utils/testUtils';
 import { WorkPacketColumn } from './WorkPacketColumns';
-import { SortDirection } from 'src/data/services/graphql';
+import { WorkPacketCommandType, WorkPacketStatus, WorkStatus, WorkStep } from 'src/data/services/graphql';
+import { TableFiltersType } from 'src/hooks/useTableFilters';
+import { StoreProvider } from 'easy-peasy';
+import store from 'src/store';
+import { ROUTES_ID } from 'src/data/constants/RouteConstants';
+import { endOfToday, startOfYesterday } from 'date-fns';
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useLocation: () => ({
     pathname: 'localhost:3000/archives?endDate=2021-11-28&filter=&orgSid=280&startDate=2013-12-26',
   }),
+  useHistory: () => ({
+    replace: jest.fn(),
+  }),
 }));
 
+const tableFilters = (includeToday: boolean): TableFiltersType => {
+  const sDate = includeToday? startOfYesterday() : new Date(2020, 10, 13);
+  const eDate = includeToday? endOfToday() : new Date(2020, 10, 14);
+
+  return {
+    startDate: {value: sDate, setValue: jest.fn(), onChange: jest.fn()},
+    endDate: {value: eDate, setValue: jest.fn(), onChange: jest.fn()},
+    searchText: {
+      value: '',
+      label: '',
+      delayedValue: '',
+      type: 'text',
+      placeholder: '',
+      setValue: jest.fn(),
+      onChange: jest.fn(),
+    },
+    pagingParams: {},
+    setPagingParams: jest.fn(),
+  };
+};
+
+let lazyQueryCalled = false;
+const data = {
+  wpProcessErrors: {
+    listPageInfo: {
+      pageHeaderLabel: 'ABC File Status',
+      secondaryHeaderLabel: 'ABC Archives',
+    },
+    paginationInfo: {
+      totalPages: 2,
+      totalElements: 120,
+      pageNumber: 0,
+      pageSize: 100,
+    },
+  },
+};
+const emptyData = {
+  wpProcessErrors: {
+    paginationInfo: {
+      totalPages: 0,
+      totalElements: 0,
+      pageNumber: 0,
+      pageSize: 100,
+    },
+  },
+  listPageInfo: {
+    pageHeaderLabel: 'ABC File Status',
+    secondaryHeaderLabel: 'ABC Archives',
+  },
+};
+const loading = false;
+const mockGraphQLLazy = () => [
+  () => {
+    lazyQueryCalled = true;
+  },
+  {
+    data,
+    loading,
+    error: undefined,
+  },
+];
+
+const mockEmptyGraphQLLazy= () => [
+  () => {
+    lazyQueryCalled = true;
+  },
+  {
+    data: emptyData,
+    loading,
+    error: undefined,
+  },
+];
+
+const generateData = () => {
+  const data: WorkPacketStatus[] = [];
+  for (let i = 0; i < 120; i++) {
+    data.push({
+      inboundFilename: 'ABC-DEF-PROD.zip',
+      orgSid: '22',
+      orgId: 'ABC',
+      packetStatus: WorkStatus.Complete,
+      step: WorkStep.TransmitFile,
+      stepStatus: WorkStatus.Complete,
+      vendorSid: '55',
+      vendorId: 'DEF',
+      workOrderId: `2020-11-22-work111${i}`,
+      vendorFilename: 'outbound.zip',
+      timestamp: '2020-11-22T13:55:23.000',
+      commands: [
+        {
+          commandType: WorkPacketCommandType.ViewDetails,
+          label: 'View',
+        },
+        {
+          commandType: WorkPacketCommandType.DownloadFile,
+          label: 'Download',
+        },
+      ],
+    });
+  }
+  return data;
+};
+
+const onLoading = jest.fn();
+const onItemsListChange = jest.fn();
+const setContextualTitle = jest.fn();
 const defaultProps = {
   id: '',
   cols: [
@@ -19,109 +133,130 @@ const defaultProps = {
     WorkPacketColumn.CLIENT_FILE,
     WorkPacketColumn.VENDOR_FILE,
   ],
-  lazyQuery: null,
-  getItems: [] as any,
+  lazyQuery: mockGraphQLLazy,
+  getItems: generateData,
   searchTextPlaceholder: 'Extract Name, Status, Vendor, etc.',
-  defaultSort: [
-    {
-      property: 'timestamp',
-      direction: SortDirection.Desc,
-      nullHandling: null,
-      ignoreCase: true,
-    },
-  ],
-  onLoading: jest.fn(),
-  onItemsListChange: jest.fn(),
+  routeId: ROUTES_ID.FILE_STATUS,
+  setContextualTitle,
+  onLoading,
+  onItemsListChange,
+  tableFilters: tableFilters(true),
 };
 
+const emptyTableProps = {
+  ...defaultProps,
+  getItems: () => [],
+  lazyQuery: mockEmptyGraphQLLazy,
+};
+
+jest.mock('src/hooks/useOrgSid', () => ({
+  useOrgSid: () => ({
+    orgSid: 1,
+  }),
+}));
+
+jest.mock('src/utils/ErrorHandler', () => ({
+  ErrorHandler: () => {
+    return () => {
+      // do nothing ;
+    }
+  }
+}));
+
 describe('Work Packet Table Container Testing Unit...', () => {
-  const mountedComponent = shallowWithTheme(
-    <WorkPacketTable
-      tableFilters={{
-        startDate: { value: new Date(), setValue: jest.fn(), onChange: jest.fn() },
-        endDate: { value: new Date(), setValue: jest.fn(), onChange: jest.fn() },
-        searchText: {
-          value: '',
-          label: '',
-          delayedValue: '',
-          type: 'text',
-          placeholder: '',
-          setValue: jest.fn(),
-          onChange: jest.fn(),
-        },
-        pagingParams: {},
-        setPagingParams: jest.fn(),
-      }}
-      {...defaultProps}
-    />
-  );
+  it ('Table is rendered', () => {
+    const wrapper = mountWithTheme(
+      <StoreProvider store={store}>
+        <WorkPacketTable {...defaultProps} />
+      </StoreProvider>
+    );
 
-  // it('renders without error', () => {
-  //   const component = TestRenderer.create(
-  //     <MockedProvider mocks={mocks} addTypename={false}>
-  //       <Component {...defaultProps} lazyQuery={} />
-  //     </MockedProvider>
-  //   );
+    expect(lazyQueryCalled).toEqual(true);
+    expect(onLoading).toBeCalled();
+    expect(onItemsListChange).toBeCalled();
+    expect(setContextualTitle).toBeCalled();
+    expect(wrapper.find('TableFilters')).toHaveLength(1);
+    expect(wrapper.find('ScrollableTable')).toHaveLength(1);
+    expect(wrapper.find('Paginator')).toHaveLength(1);
+    expect(wrapper.find('span[id="__Paginator_PagingInfo-Text"]')).toHaveLength(1);
 
-  //   const tree = component.toJSON();
-  //   expect(tree.children).toContain('Loading data');
-  // });
+    // There should be 5 columns rendered
+    expect(wrapper.find('.ms-DetailsHeader-cellTitle')).toHaveLength(5);
+    // Timestamp should be sorted by default
+    expect(wrapper.find('.ms-DetailsHeader-cellTitle').at(0).find('FontIcon[iconName="SortDown"]')).toHaveLength(1);
+    // Click Timestamp column to sort ascending
+    wrapper.find('.ms-DetailsHeader-cellTitle').at(0).simulate('click');
+    expect(wrapper.find('.ms-DetailsHeader-cellTitle').at(0).find('FontIcon[iconName="SortUp"]')).toHaveLength(1);
+    // Now sort a different column
+    wrapper.find('.ms-DetailsHeader-cellTitle').at(1).simulate('click');
+    expect(wrapper.find('.ms-DetailsHeader-cellTitle').at(1).find('FontIcon[iconName="SortUp"]')).toHaveLength(1);
+    expect(wrapper.find('.ms-DetailsHeader-cellTitle').at(0).find('FontIcon')).toHaveLength(0);
 
-  // it('bbbbb', () => {
-  //   const component = mountWithTheme(
-  //     <MockedProvider mocks={mocks} addTypename={false}>
-  //       <Component {...defaultProps} id="TableArchive"></Component>
-  //     </MockedProvider>
-  //   );
+    // Click on a details link
+    expect(wrapper.find('div.ms-DetailsRow-cell')).toHaveLength(50);
+    wrapper.find('div.ms-DetailsRow-cell button').at(0).simulate('click');
 
-  //   // const tree = component.toJSON();
-  //   // expect(component).toContain('Loading data');
-  //   const searchId = component.find('#TableArchive_TableWrap');
-  //   expect(searchId.length).toBe(1);
-  //   expect(toJSON(component)).toMatchSnapshot();
-  // });
+    // Change pages
+    const nextButton = wrapper.find('button[id="__Paginator_Next"]');
+    expect(nextButton).toHaveLength(1);
+    nextButton.simulate('click');
+    // Table Filters should change
 
-  // it('ccccc', () => {
-  //   const component = shallowWithTheme(
-  //     <MockedProvider mocks={mocks} addTypename={false}>
-  //       <Component {...defaultProps} lazyQuery={useWorkPacketStatusesLazyQuery} />
-  //     </MockedProvider>
-  //   );
-
-  //   const searchId = component.find('#__spanError');
-  //   expect(searchId.length).toBe(1);
-  //   expect(toJSON(component)).toMatchSnapshot();
-  // });
-
-  // it('ddddd', () => {
-  //   const component = shallowWithTheme(
-  //     <MockedProvider mocks={mocks} addTypename={false}>
-  //       <Component {...defaultProps} lazyQuery={useWorkPacketStatusesLazyQuery} />
-  //     </MockedProvider>
-  //   );
-
-  //   const searchId = component.find('#__StyledSpacingId');
-  //   expect(searchId.length).toBe(1);
-  //   expect(toJSON(component)).toMatchSnapshot();
-  // });
-
-  it('Should be defined', () => {
-    expect(mountedComponent).toBeDefined();
   });
 
-  it('Should render correctly', () => {
-    expect(mountedComponent).toMatchSnapshot();
+  it ('Empty Polling Table', () => {
+    const startPolling = jest.fn();
+    const stopPolling = jest.fn();
+    const pollingQuery = () => ({
+      startPolling,
+      stopPolling,
+      data: {
+        workPacketStatusPoll: 0,
+      },
+      loading,
+      error: undefined,
+    });
+
+    const wrapper = mountWithTheme(
+      <StoreProvider store={store}>
+        <WorkPacketTable {...emptyTableProps} pollingQuery={ pollingQuery }/>
+      </StoreProvider>
+    );
+
+    expect(lazyQueryCalled).toEqual(true);
+    expect(wrapper.find('TableFilters')).toHaveLength(1);
+    expect(wrapper.find('ScrollableTable')).toHaveLength(1);
+    expect(wrapper.find('EmptyState')).toHaveLength(1);
+    expect(wrapper.find('span[id="__Paginator_PagingInfo-Text"]')).toHaveLength(0);
+    expect(startPolling).toBeCalled();
+    expect(stopPolling).toHaveBeenCalledTimes(0);
   });
 
-  // it('Should find Id TableArchive_TableWrap', () => {
-  //   const wrapper = mountWithTheme(
-  //     <StoreProvider store={store}>
-  //       <ApolloContextProvider>
-  //         <Component {...defaultProps} id="TableArchive"></Component>
-  //       </ApolloContextProvider>
-  //     </StoreProvider>
-  //   );
-  //   const searchId = wrapper.find('#TableArchive_TableWrap');
-  //   expect(searchId.length).toBe(1);
-  // });
+  it ('No Polling Outside of Today', () => {
+    const startPolling = jest.fn();
+    const stopPolling = jest.fn();
+    const pollingQuery = () => ({
+      startPolling,
+      stopPolling,
+      data: {
+        workPacketStatusPoll: 0,
+      },
+      loading,
+      error: undefined,
+    });
+
+    const wrapper = mountWithTheme(
+      <StoreProvider store={store}>
+        <WorkPacketTable {...emptyTableProps} pollingQuery={ pollingQuery } tableFilters={tableFilters(false)}/>
+      </StoreProvider>
+    );
+
+    expect(lazyQueryCalled).toEqual(true);
+    expect(wrapper.find('TableFilters')).toHaveLength(1);
+    expect(wrapper.find('ScrollableTable')).toHaveLength(1);
+    expect(wrapper.find('EmptyState')).toHaveLength(1);
+    expect(wrapper.find('span[id="__Paginator_PagingInfo-Text"]')).toHaveLength(0);
+    expect(startPolling).toHaveBeenCalledTimes(0);
+    expect(stopPolling).toHaveBeenCalledTimes(1);
+  });
 });
