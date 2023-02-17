fadac7dad4cd9e0091e0d84f27e78ed19d4a4618
Oscar Vallin <oscar.hernandez@bpo-technologies.com>
2023-02-17 10:32:48 -0600

    Linter fixes

--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -5,10 +5,16 @@ import { theme } from '../styles/themes/theme';
 
 // eslint-disable-next-line react/prop-types
 const LightTheme = { ...theme, ...defaultTheme };
-export const mountWithTheme = (children: any) => mount(<ThemeProvider theme={LightTheme}>{children}</ThemeProvider>);
+export const mountWithTheme = (children: any) => mount(
+  <ThemeProvider theme={LightTheme}>{children}</ThemeProvider>,
+)
 
-export const shallowWithTheme = (children: any) => shallow(<ThemeProvider theme={LightTheme}>{children}</ThemeProvider>);
+export const shallowWithTheme = (children: any) => shallow(
+  <ThemeProvider theme={LightTheme}>{children}</ThemeProvider>,
+);
 
-export const renderWithTheme = (children: any) => render(<ThemeProvider theme={LightTheme}>{children}</ThemeProvider>);
+export const renderWithTheme = (children: any) => render(
+  <ThemeProvider theme={LightTheme}>{children}</ThemeProvider>,
+);
 
 export * from '@testing-library/react';
