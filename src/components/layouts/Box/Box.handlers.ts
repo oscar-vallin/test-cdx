e0ffcf6e69d3150461adea48d7ca6d3f8b58118b
Oscar Vallin <oscar.hernandez@bpo-technologies.com>
2023-02-17 09:49:25 -0600

    Linter fixes

--- src/components/layouts/Box/Box.handlers.ts
+++ src/components/layouts/Box/Box.handlers.ts
@@ -1,5 +1,6 @@
 import { StyleConstants } from '../../../data/constants/StyleConstants';
 
-export const validateDirection = (direction) => (direction !== StyleConstants.DIRECTION_ROW && direction !== StyleConstants.DIRECTION_COLUMN
-  ? direction
-  : StyleConstants.DIRECTION_COLUMN);
+export const validateDirection = (direction) => (
+  direction !== StyleConstants.DIRECTION_ROW && direction !== StyleConstants.DIRECTION_COLUMN
+    ? direction
+    : StyleConstants.DIRECTION_COLUMN);
